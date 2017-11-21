9011480d3b4c30b9b0c83aecfe4e140b2747ac64
emmapankey <emmapankey19@gmail.com>
2017-11-21 14:37:10 -0500

    started to write in README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # liri-node-app
 command line nodejs app
+
+    INSTRUCTIONS:
+
+     In the terminal type one of the following commands:
+     1) node liri.js show-tweets '<twitter username here>' (Username can be any existing twitter name. If no username is entered the default value is my username.)
+     2) node liri.js spotify-this-song '<song name here>' (If no song name is entered the default value is "The Sign" by Ace of Base.)
+     3) node liri.js movie-this '<movie title here' (If no movie title is entered the default value is "Mr. Nobody".)
+     4) node liri.js do-what-it-says
--- liri.js
+++ liri.js
@@ -1,13 +1,3 @@
-// INSTRUCTIONS:
-
-// In the terminal type one of the following commands:
-// 1) node liri.js show-tweets '<twitter username here>' (Username can be any existing twitter name. If no username is entered the default value is my username.)
-// 2) node liri.js spotify-this-song '<song name here>' (If no song name is entered the default value is "The Sign" by Ace of Base.)
-// 3) node liri.js movie-this '<movie title here' (If no movie title is entered the default value is "Mr. Nobody".)
-// 4) node liri.js do-what-it-says
-
-//<-------------------------------------------------------------------------------------------------------------------------->
-
 // Load the fs package to read and write
 var fs = require("fs");
 
@@ -76,7 +66,7 @@ function twitterFunction() {
 // Calls the Spotify API and 
 function spotifyFunction() {
 
-    // Import the Spotify node module
+    // Import the Spotify node module and returns song data
     var Spotify = require('node-spotify-api');
 
     var client = new Spotify({
